7718a49ddcab3083f350dfc8ce12275eb23f9e80
yoljasro <saidaliyevjasur450@gmail.com>
2024-08-06 14:03:16 +0500

    first commit

---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import styles from './index.module.sass';
+import Image from 'next/image';
+
+export const Navbar = () => {
+
+    return (
+        <div className={styles.navbar}>
+            <Image className={styles.navbar__logo} src={'/assets/img/logo.svg'} alt='logo' width={118} height={38}/>
+            <ul className={styles.navbar__menu}>
+                <li>Home</li>
+                <li>About us</li>
+                <li>Projects</li>
+                <li>Services</li>
+            </ul>
+            <button className={styles.navbar__btn}>Contact us </button>
+        </div>
+    );
+};
