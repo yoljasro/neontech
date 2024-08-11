a38ee40ad9c93bd37e3d42c352f4eaf9a077b5bb
yoljasro <saidaliyevjasur450@gmail.com>
2024-08-11 15:39:47 +0500

    changs

--- src/components/Services/index.tsx
+++ src/components/Services/index.tsx
@@ -8,6 +8,31 @@ export const Services = () => {
         <div className={styles.services}>
             <p className={styles.services__title}>OUR SERVICES</p>
             <p className={styles.services__desc}>Our agency experts will provide you <br /> with a full stack of services</p>
+            <div className={styles.services__cards}>
+                <div className={styles.services__card}>
+                    <Image src={'/assets/img/service1.svg'} alt='service' width={144} height={144}/>
+                    <p className={styles.services__name}>Development <br /> services</p>
+                    <p className={styles.services__subTitle}>Lorem ipsum dolor sit amet, consectetur adipiscing elit.  </p>
+                    <p className={styles.services__learn}> Learn more</p>
+                </div>
+
+                <div className={styles.services__card}>
+                    <Image src={'/assets/img/service2.svg'} alt='service' width={144} height={144}/>
+                    <p className={styles.services__name}>Marketing services</p>
+                    <p className={styles.services__subTitle}>Lorem ipsum dolor sit amet, consectetur adipiscing elit.  </p>
+                    <p className={styles.services__learn}> Learn more</p>
+
+                </div>
+
+                <div className={styles.services__card}>
+                    <Image src={'/assets/img/service3.svg'} alt='service' width={144} height={144}/>
+                    <p className={styles.services__name}>Design services</p>
+                    <p className={styles.services__subTitle}>Lorem ipsum dolor sit amet, consectetur adipiscing elit.  </p>
+                    <p className={styles.services__learn}> Learn more</p>
+            <button className={styles.services__btn}>Explore our services   <GrLinkNext className={styles.services__icon} /></button>
+
+                </div>
+            </div>
         </div>
     );
 };
