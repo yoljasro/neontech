9b78ade66ac54a1ac31723929841cc468e4a3e44
yoljaro <saidaliyevjasur450@gmail.com>
2024-08-13 17:17:48 +0500

    changes

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -2,11 +2,34 @@ import React from 'react';
 import styles from './index.module.sass';
 import Image from 'next/image';
 import { GrLinkNext } from "react-icons/gr";
+import { FaFacebookF, FaTwitter } from "react-icons/fa";
+import { IoLogoInstagram, IoLogoLinkedin } from "react-icons/io5";
+
 
 export const Footer = () => {
 
     return (
         <div className={styles.footer}>
+            <div className={styles.footer__section}></div>
+            <div className={styles.footer__container}>
+                <p className={styles.footer__title}>Ready for your project</p>
+                <button className={styles.footer__btn}>Get in touch</button>
+                <div className={styles.footer__content}>
+                    <Image className={styles.navbar__logo} src={'/assets/img/logo.svg'} alt='logo' width={236} height={76} />
+                    <ul className={styles.footer__menu}>
+                        <li>Home</li>
+                        <li>About us </li>
+                        <li>Blog</li>
+                        <li>Services</li>
+                    </ul>
+                </div>
+                <div className={styles.footer__socials}>
+                    <FaFacebookF />
+                    <FaTwitter />
+                    <IoLogoInstagram/>
+                    <IoLogoLinkedin/>
+                </div>
+            </div>
         </div>
     );
 };
