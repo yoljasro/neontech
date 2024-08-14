6a1b2ce4edcefeeaa45f77cc6670698cf8090318
yoljasro <saidaliyevjasur450@gmail.com>
2024-08-14 12:57:16 +0500

    adaptive

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -7,7 +7,7 @@ export const Header = () => {
 
     return (
         <div className={styles.header}>
-            <Image src={'/assets/img/header.svg'} alt='header' width={680} height={152}></Image>
+            <Image className={styles.header__img} src={'/assets/img/header.svg'} alt='header' width={680} height={152}></Image>
             <div className={styles.header__content}>
                 <button className={styles.header__btn}>Explore our services   <GrLinkNext className={styles.header__icon} /></button>
             <Image className={styles.header__contentimg} src={'/assets/img/content.svg'} alt='contentimage' width={157} height={30}></Image>
